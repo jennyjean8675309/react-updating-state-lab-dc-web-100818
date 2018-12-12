096d49120edf1df57e29d16c0cabc7dccc4f0dba
jennyjean <jennyjean8675309@yahoo.com>
2018-12-12 13:51:51 -0500

    Updated state using setState() and the spread operator to update nested attributes

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+class DigitalClicker extends Component {
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  incrementButton = () =>{
+    let newNumber = this.state.timesClicked + 1
+    this.setState({
+      timesClicked: newNumber
+    })
+  }
+  render(){
+    return <button onClick={this.incrementButton}>{this.state.timesClicked}</button>
+  }
+}
+
+export default DigitalClicker
